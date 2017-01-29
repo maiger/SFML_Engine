c1252450692e5b1e84505c5753c95e2caa0d26b9
maiger <mage_ege@hotmail.com>
2017-01-29 16:37:32 +0200

    Added a random number generator

---
+++ Source/Util/Random.cpp
@@ -0,0 +1,26 @@
+#include "Random.h"
+
+#include <random>
+#include <ctime>
+
+namespace Random
+{
+    std::minstd_rand gen;
+
+    void init()
+    {
+        gen.seed(std::time(nullptr));
+    }
+
+    int intInRange(int low, int high)
+    {
+        std::uniform_int_distribution<int> dist(low, high);
+        return dist(gen);
+    }
+
+    float floatInRange(float low, float high)
+    {
+        std::uniform_real_distribution<float> dist(low, high);
+        return dist(gen);
+    }
+}
---
+++ Source/Util/Random.h
@@ -0,0 +1,13 @@
+#ifndef RANDOM_H_INCLUDED
+#define RANDOM_H_INCLUDED
+
+namespace Random
+{
+    void init();
+
+    int intInRange(int low, int high);
+
+    float floatInRange(float low, float high);
+}
+
+#endif // RANDOM_H_INCLUDED
